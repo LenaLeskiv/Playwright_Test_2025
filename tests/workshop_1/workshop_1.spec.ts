9f0111c495fb4d102dad2c1d7ef5119de4ba57e4
LenaLeskiv <lenaleskiv18@gmail.com>
2026-03-25 21:51:51 +1100

    workshop1_remove_skip

--- tests/workshop_1/workshop_1.spec.ts
+++ tests/workshop_1/workshop_1.spec.ts
@@ -1,13 +1,13 @@
 import {test} from '@playwright/test';
 
-test.skip('Basic Navigation', async ({page})=>{
+test('Basic Navigation', async ({page})=>{
     await page.goto('https://github.com/');
     await page.waitForTimeout(3000);
     await page.reload();
 })
 
 
-test.skip('Interaction with Web Elements on GitHub', async ({page})=>{
+test('Interaction with Web Elements on GitHub', async ({page})=>{
     await page.goto('https://github.com/');
     //await page.locator('//a[contains(@class, "HeaderMenu-link--sign-in")]').click();
     //this method can be used without xpath locator - playwright has getBuRole method
